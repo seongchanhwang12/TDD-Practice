46805bc8229cd0edf631ef59c1979e0180af1de8
seongchanhwang12 <tjdcksghkd12@gmail.com>
2025-10-17 00:16:12 +0900

    feat : 특정 유저 포인트 조회 usecase 성공 케이스 구현

---
+++ src/main/java/io/hhplus/tdd/point/usecase/GetUserPointUseCase.java
@@ -0,0 +1,18 @@
+package io.hhplus.tdd.point.usecase;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.UserPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GetUserPointUseCase {
+
+    private final UserPointTable userPointTable;
+
+    public UserPoint handle(long userId) {
+        return userPointTable.selectById(userId);
+    }
+}
---
+++ src/test/java/io/hhplus/tdd/point/usecase/GetUserPointUseCaseTest.java
@@ -0,0 +1,39 @@
+package io.hhplus.tdd.point.usecase;
+
+import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.UserPoint;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+
+class GetUserPointUseCaseTest {
+
+    UserPointTable mockRepo = mock(UserPointTable.class);
+    GetUserPointUseCase sut = new GetUserPointUseCase(mockRepo);
+
+    @Test
+    @DisplayName("Given 유효한 userId, When 포인트를 조회하면 Then 유저 포인트를 반환한다")
+    void givenValidUserId_whenGetUserPoint_thenReturnUserPoint() {
+        long userId = 1L;
+        long point = 1L;
+
+        UserPoint userPoint = new UserPoint(userId, point, System.currentTimeMillis());
+        given(mockRepo.selectById(userId)).willReturn(userPoint);
+
+        //when
+        UserPoint result = sut.handle(userId);
+
+        //then
+        then(mockRepo).should().selectById(userId);
+        assertThat(result.id()).isEqualTo(userPoint.id());
+        assertThat(result.point()).isEqualTo(userPoint.point());
+
+    }
+}
